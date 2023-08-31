22bd6e821a33a5dea2578ac9d2a6150fc5a1609c
Harshit Thummar <itsharshitthumar@gmail.com>
2023-08-31 15:00:02 +0530

    added moving saw in the game

--- scripts/enemy_sideway.cs
+++ scripts/enemy_sideway.cs
@@ -4,7 +4,44 @@ using UnityEngine;
 
 public class enemy_sideway : MonoBehaviour
 {
+    [SerializeField] private float movementdistance;
+    [SerializeField] private float speed;
     [SerializeField] private float damage;
+
+    private float leftedge;
+    private float rightedge;
+    private bool movingleft;
+
+    private void Awake()
+    {
+        leftedge = transform.position.x - movementdistance;
+        rightedge = transform.position.y + movementdistance;
+    }
+    private void Update()
+    {
+        if (movingleft)
+        {
+            if(transform.position.x > leftedge)
+            {
+                transform.position=new Vector3(transform.position.x-speed*Time.deltaTime,transform.position.y,transform.position.z);
+            }
+            else
+            {
+                movingleft = false;
+            }
+        }
+        else
+        {
+            if (transform.position.x < rightedge)
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                movingleft = true;
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
