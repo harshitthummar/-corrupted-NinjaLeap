e238774c381d040b6b738dc8a2c10aba244e1a16
Harshit Thummar <itsharshitthumar@gmail.com>
2023-09-03 16:08:02 +0530

    added a knight enemy and its animations

--- scripts/core/cameracontroller.cs
+++ scripts/core/cameracontroller.cs
@@ -18,12 +18,13 @@ public class cameracontroller : MonoBehaviour
     private void Update()
     {
         //room camera movement
-        //transform.position = Vector3.SmoothDamp(transform.position,new Vector3(currentposx,transform.position.y,transform.position.z),ref velocity,speed * Time.deltaTime);
+       // transform.position = Vector3.SmoothDamp(transform.position,new Vector3(currentposx,transform.position.y,transform.position.z),ref velocity,speed);
 
+        //follow player
         transform.position = new Vector3(player.position.x + lookahead,transform.position.y,transform.position.z);
-        Mathf.Lerp(lookahead, (aheaddistance * player.localScale.x),Time.deltaTime * camspeed);
+        lookahead= Mathf.Lerp(lookahead, (aheaddistance * player.localScale.x),Time.deltaTime * camspeed);
     }
-    public void movetonewroom(Transform _newroom)
+    public void movetonewroom(Transform _newroom) 
     {
         currentposx = _newroom.position.x;
     }
--- scripts/room/door.cs
+++ scripts/room/door.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class door : MonoBehaviour
+public class door : room
 {
     [SerializeField] Transform previousroom;
     [SerializeField] Transform nextroom;
     [SerializeField] cameracontroller cam;
 
+    private void Awake()
+    {
+        cam = Camera.main.GetComponent<cameracontroller>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
@@ -15,14 +19,15 @@ public class door : MonoBehaviour
             if (collision.transform.position.x < transform.position.x)
             {
                 cam.movetonewroom(nextroom);
-                nextroom.GetComponent<room>().activeroom(true);
-                previousroom.GetComponent<room>().activeroom(false);
+             //  nextroom.GetComponent<room>().activeroom(true);
+              // previousroom.GetComponent<room>().activeroom(false);
             }
             else
             {
                 cam.movetonewroom(previousroom);
-                nextroom.GetComponent<room>().activeroom(false);
-                previousroom.GetComponent<room>().activeroom(true);
+              // previousroom.GetComponent<room>().activeroom(true);
+              // nextroom.GetComponent<room>().activeroom(false);
+              
             }
                  
         }
